8cf6649fda785a909cc3c616731c363b8631f257
Felipe S. Neves <felipe.s.neves@outlook.com>
2024-07-24 23:47:36 -0300

    Introdução  SQL injection

--- App/model/Cursos.php
+++ App/model/Cursos.php
@@ -20,8 +20,11 @@
             $where = empty($condicaoWhere) ? '' : "WHERE {$condicaoWhere}";
             $sql = "SELECT $indicesSelect FROM {$this->tabela} {$where} ORDER BY titulo_curso ASC";
 
-            $stm = $this->conexao->query($sql);
+            $stm = $this->conexao->prepare($sql);
             
+            if($stm->execute() === false) { return array(); }
+
             return $stm->fetchAll(PDO::FETCH_ASSOC);
+ 
         }
     }
